4a270c5486cbbe372e549ac2f523bf6691369294
DanielYouCan <danbizarre@gmail.com>
2018-02-06 18:13:16 +0700

    Answer model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
+  has_many :answers
+
   validates :body,:title, presence: true
-  
+
 end
---
+++ spec/factories/answers.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :answer do
+    title "MyString"
+    body "MyString"
+    Question nil
+  end
+end
