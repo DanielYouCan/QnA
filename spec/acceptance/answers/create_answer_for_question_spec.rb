ad3e14d479b21db63a4d49dbd8549e1e8548e782
DanielYouCan <danbizarre@gmail.com>
2018-02-15 00:09:28 +0700

    Create answer test fix

--- spec/acceptance/answers/create_answer_for_question_spec.rb
+++ spec/acceptance/answers/create_answer_for_question_spec.rb
@@ -14,7 +14,7 @@ feature 'User adds answer to question', %q{
 
     visit question_path(question)
 
-    fill_in 'Body', with: 'My unique answer'
+    fill_in 'answer[body]', with: 'My unique answer'
     click_on 'Answer'
 
     within '.answers' do
@@ -33,7 +33,7 @@ feature 'User adds answer to question', %q{
     sign_in(user)
     visit question_path(question)
 
-    fill_in 'Body', with: 'Abc'
+    fill_in 'answer[body]', with: 'Abc'
     click_on 'Answer'
 
     expect(page).to have_content 'Body is too short'
