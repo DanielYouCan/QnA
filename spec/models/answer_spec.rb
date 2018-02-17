77c328050f62a1cee96d9b217d49d1b300106ef0
DanielYouCan <danbizarre@gmail.com>
2018-02-17 15:27:04 +0700

    Polymorphic attachments. Answers assosiation with attachments

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,11 +4,16 @@ RSpec.describe Answer, type: :model do
   context 'assosiation' do
     it { should belong_to(:question) }
     it { should belong_to(:user) }
+    it { should have_many(:attachments) }
   end
 
   context 'validation' do
     it { should validate_presence_of(:body) }
     it { should validate_length_of(:body).is_at_least(5) }
+  end
+
+  context 'nested attributes' do
+    it { should accept_nested_attributes_for :attachments }
   end
 
   describe '#set_best' do
