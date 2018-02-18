2f54adc6300f5b3d7aa08cc22c9d2e4debdb31a7
DanielYouCan <danbizarre@gmail.com>
2018-02-18 15:24:03 +0700

    Multiple attachments to question and answer

--- spec/acceptance/files/add_files_to_answer_spec.rb
+++ spec/acceptance/files/add_files_to_answer_spec.rb
@@ -24,4 +24,18 @@ feature 'Adds file to answer', %q{
     end
   end
 
+  scenario 'User adds multiple files to an answer', js: true do
+    fill_in 'answer[body]', with: 'My unique answer'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    find('.octicon-plus').click
+    inputs = all('input[type="file"]')
+    inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
+    click_on 'Answer'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+    end
+  end
+
 end
