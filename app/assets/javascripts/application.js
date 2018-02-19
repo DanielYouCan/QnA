7c7b211fed5c1052b62abc3418f3372dee854007
DanielYouCan <danbizarre@gmail.com>
2018-02-19 13:34:46 +0700

    Edit question link fix and using remotipart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery3
-//= require jquery_ujs
 //= require rails-ujs
 //= require cocoon
+//= require jquery.remotipart
 //= require turbolinks
 //= require_tree .
