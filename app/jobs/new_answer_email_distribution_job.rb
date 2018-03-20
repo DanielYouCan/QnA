0f0339a71844c4b6d0d9114ee1c96147a1b3c87f
DanielYouCan <danbizarre@gmail.com>
2018-03-20 18:14:35 +0700

    Remove extra line

--- app/jobs/new_answer_email_distribution_job.rb
+++ app/jobs/new_answer_email_distribution_job.rb
@@ -2,8 +2,6 @@ class NewAnswerEmailDistributionJob < ApplicationJob
   queue_as :mailers
 
   def perform(question, answer)
-    return false if question.subscribers.empty?
-    
     question.subscribers.find_each do |subscriber|
       AnswerMailer.distribution(subscriber, answer).try(:deliver_later)
     end
